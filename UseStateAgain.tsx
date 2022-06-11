9d2b9ea5dcb166a0c6cb1657c6381750295b08af
codeztech-atique <atique1224@gmail.com>
2022-06-11 14:16:09 +0530

    1 file modified

--- UseStateAgain.tsx
+++ UseStateAgain.tsx
@@ -6,23 +6,6 @@ import './style.css';
 
 export default function UseStateAgain() {
   const [count, setCount] = React.useState(0); // For use UseState
-
-  // const counterReducer = (state, action) => {
-  //   switch (action.type) {
-  //     case 'RESET':
-  //       return { count: 0 };
-  //     case 'INC':
-  //       return { count: state.count + 1 };
-  //     case 'DEC':
-  //       return { count: state.count - 1 };
-  //     default:
-  //       throw new Error('action not found');
-  //   }
-  //   return state;
-  // };
-
-  // const [state, dispatch] = React.useReducer(counterReducer, { count: 0 });
-
   return (
     <div>
       <h1>Using useState check this before coming useReducer- </h1>
